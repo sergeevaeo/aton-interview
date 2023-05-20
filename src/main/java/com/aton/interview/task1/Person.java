6c93d6fe4b4a781a4279047556f912012f893dfe
Ekaterina Sergeeva <katy.o.sergeeva@gmail.com>
2023-05-20 23:46:06 +0300

    Added requireNonNull in Person class and added task2Answer

--- src/main/java/com/aton/interview/task1/Person.java
+++ src/main/java/com/aton/interview/task1/Person.java
@@ -5,29 +5,34 @@ import net.jcip.annotations.NotThreadSafe;
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.Objects.requireNonNull;
+
 @NotThreadSafe
 public class Person extends Thread {
     private final Order order;
     private final String name;
-    private final List<String> lines;
+    private final List<String> phrases;
 
     public Person(Order order, String name) {
-        this.name = name;
-        this.order = order;
-        lines = new LinkedList<>();
+        this.order = requireNonNull(order);
+        this.name = requireNonNull(name);
+        this.phrases = new LinkedList<>();
     }
 
+    /**
+     * This method adds a new phrase to the person. Should not be called outside the person context (not thread safe).
+     */
     public void addPhrase(String phrase) {
-        lines.add(phrase);
+        phrases.add(phrase);
     }
 
     @Override
     public void run() {
         synchronized (order) {
-            while (!order.getPeopleOrder().isEmpty()) {
+            while (!order.getPeopleOrder().isEmpty() && !phrases.isEmpty()) {
                 if (order.getPerson().equals(name)) {
-                    System.out.println(name + ": " + lines.remove(0));
-                    order.removePerson(); // удаление из очереди
+                    System.out.println(name + ": " + phrases.remove(0));
+                    order.removePerson();
                     order.notifyAll();
                 } else {
                     try {
